aa5317d6ac402d4e05c162d4fd497ca3bb86d09d
yayanbachtiar <abiedoank@gmail.com>
2019-05-31 16:11:59 +0700

    add circleci

--- src/app.js
+++ src/app.js
@@ -3,18 +3,37 @@ import { HOST, PORT, NODE_ENV } from '~/env'
 import swaggerDefinition from '~/core/swagger-deff'
 import swaggerJSDoc from 'swagger-jsdoc'
 import swaggerUi from 'swagger-ui-express'
+// middleware
+import cors from 'cors'
+import helmet from 'helmet'
+import compression from 'compression'
+import slowDown from 'express-slow-down'
+
 const app = express()
+
+// MIDDLEWARE
+app.use(cors())
+app.use(helmet())
+app.use(compression())
+app.use(slowDown({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    delayAfter: 100, // allow 100 requests per 15 minutes, then...
+    delayMs: 500 // begin adding 500ms of delay per request above 100:
+    // request # 101 is delayed by  500ms
+    // request # 102 is delayed by 1000ms
+    // request # 103 is delayed by 1500ms
+}));
+
+
+/**
+ * Swagger configuration
+ */
 const options = {
-    // Import swaggerDefinitions
     swaggerDefinition,
-    // Path to the API docs
-    // Note that this path is relative to the current directory from which the Node.js is ran, not the application itself.
     apis: ['./modules/*/rest.js'],
 };
-
 const swaggerSpec = swaggerJSDoc(options);
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
--- src/modules/hello-world/rest.js
+++ src/modules/hello-world/rest.js
@@ -3,5 +3,6 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (req, res, next) => {
-    res.send('hello wolrd')
+    res.send('Hello, World!')
 })
+export default router;
