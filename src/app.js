b117a620257cdb3f137dae8b4615c06368b708c5
yayanbachtiar <user@Users-MacBook-Pro.local>
2019-07-31 11:40:04 +0700

    fixing eslint

--- src/app.js
+++ src/app.js
@@ -1,27 +1,27 @@
 import express from 'express';
-import { HOST, PORT, NODE_ENV } from '~/env'
-import swaggerDefinition from '~/core/swagger-deff'
-import swaggerJSDoc from 'swagger-jsdoc'
-import swaggerUi from 'swagger-ui-express'
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 // middleware
-import cors from 'cors'
-import helmet from 'helmet'
-import compression from 'compression'
-import slowDown from 'express-slow-down'
+import cors from 'cors';
+import helmet from 'helmet';
+import compression from 'compression';
+import slowDown from 'express-slow-down';
+import { PORT } from './env';
+import swaggerDefinition from './core/swagger-deff';
 
-const app = express()
+const app = express();
 
 // MIDDLEWARE
-app.use(cors())
-app.use(helmet())
-app.use(compression())
+app.use(cors());
+app.use(helmet());
+app.use(compression());
 app.use(slowDown({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    delayAfter: 100, // allow 100 requests per 15 minutes, then...
-    delayMs: 500 // begin adding 500ms of delay per request above 100:
-    // request # 101 is delayed by  500ms
-    // request # 102 is delayed by 1000ms
-    // request # 103 is delayed by 1500ms
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  delayAfter: 100, // allow 100 requests per 15 minutes, then...
+  delayMs: 500, // begin adding 500ms of delay per request above 100:
+  // request # 101 is delayed by  500ms
+  // request # 102 is delayed by 1000ms
+  // request # 103 is delayed by 1500ms
 }));
 
 
@@ -29,11 +29,11 @@ app.use(slowDown({
  * Swagger configuration
  */
 const options = {
-    swaggerDefinition,
-    apis: ['./modules/*/rest.js'],
+  swaggerDefinition,
+  apis: ['./modules/*/rest.js'],
 };
 const swaggerSpec = swaggerJSDoc(options);
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
--- src/core/swagger-deff.js
+++ src/core/swagger-deff.js
@@ -1,17 +1,23 @@
-import {HOST, PORT}from '~/env'
-
-const host = `http://${HOST}:${PORT}`;
-
-
 const swaggerDefinition = {
-    info: {
-        // API informations (required)
-        title: 'An example api ', // Title (required)
-        version: '1.0.0', // Version (required)
-        description: 'A sample API', // Description (optional)
+  info: {
+    // API informations (required)
+    title: 'An example api ', // Title (required)
+    version: '1.0.0', // Version (required)
+    description: 'A sample API', // Description (optional)
+  },
+  basePath: '/', // Base path (optional)
+  securityDefinitions: {
+    Bearer: {
+      type: 'apiKey',
+      name: 'Authorization',
+      in: 'header',
     },
-    host: host, // Host (optional)
-    basePath: '/', // Base path (optional)
+  },
+  security: [
+    {
+      Bearer: [],
+    },
+  ],
 };
 
 export default swaggerDefinition;
